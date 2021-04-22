0941ff5c390a70bf27e42865566d0c0f5f360bd6
ferygood <yaochung41@gmail.com>
2021-04-22 11:08:10 +0200

    add more columns in mysql database

--- data/dump.py
+++ data/dump.py
@@ -21,18 +21,30 @@ mycursor = mydb.cursor() #啟動 cursor
              image_table (id, img_url)
 """
 
+
 for n in range(0, len(df)):
     spot_id = df[n]["_id"]
+    spot_cat2 = df[n]["CAT2"]
     spot_name = df[n]["stitle"]
+    spot_describe = df[n]["xbody"]
+    spot_transport = df[n]["info"]
+    spot_mrt = df[n]["MRT"]
+    spot_lat = df[n]["latitude"]
+    spot_lon = df[n]["longitude"]
     spot_address = df[n]["address"]
+    # description
+    spot_describe_split = spot_describe.split("，")[0]
+    
     spot_img = df[n]["file"]
     #因為景點網址的開頭都是 http 所以我們用 http:// 分
     urls = spot_img.split("http://")[1:]
     http_urls = ["http://"+url for url in urls] #把 http 補回去
     
     #將資料放到資料庫內
-    sql1 = "insert into main_table (id, name, address) values (%s, %s, %s)"
-    val1 = (spot_id, spot_name, spot_address)
+    sql1 = "insert into main_table (id, name, category, description, address, transport,\
+    mrt, latitude, longitude) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val1 = (spot_id, spot_name, spot_cat2, spot_describe_split, spot_address, spot_transport,
+    spot_mrt, spot_lat, spot_lon)
     mycursor.execute(sql1, val1)
 
     sql2 = "insert into image_table (id, img) values (%s, %s)"
@@ -42,8 +54,7 @@ for n in range(0, len(df)):
     mydb.commit()
 
 
+#print(df[0].keys())
 
-#mycursor.execute("show tables;")
-#result = mycursor.fetchall()
 
 
